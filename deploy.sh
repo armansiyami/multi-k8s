839a03912224f6c0fc0a55ee2bd4c9c7326f1652
armansiyami <arman.siyami@gmail.com>
2019-07-29 15:30:51 -0700

    typos in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 docker build -t armansiyami/multi-client:latest -t armansiyami/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t armansiyami/multi-server:latest -t armansiyami/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t armansiyami/multi-worker:latest -t armansiyami/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 # Push images to docker hub
 docker push armansiyami/multi-client:latest
 docker push armansiyami/multi-server:latest
@@ -14,6 +15,6 @@ docker push armansiyami/multi-worker:$SHA
 # Apply config files from kubernetes directory
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=armansiyami/multi-server:$SHA
-kubectl set image deployments/client-deployment server=armansiyami/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=armansiyami/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=armansiyami/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=armansiyami/multi-worker:$SHA
 
